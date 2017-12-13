3cb72bcfd8124ab4805f21900c140a20cec81457
sarahdudek <sarah.marie.dudek@gmail.com>
2017-12-12 21:00:32 -0600

    Remove mistake

--- web/index.php
+++ web/index.php
@@ -20,7 +20,7 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 $app->get('/', function() use($app) {
 	$api = new CoingeckoApi();
-	$timestamp = $api->shared()->getExchangeRates(true));
+	$timestamp = $api->shared()->getExchangeRates(true);
 	$app['monolog']->addDebug('logging output.');
 	return $app['twig']->render('index.twig', array("timestamp" => $timestamp));
 });
