54c6ff59d399cae9faca3177f0cfc1658f2b7ed7
sarahdudek <sarah.marie.dudek@gmail.com>
2017-12-12 21:50:27 -0600

    Remove coingecko typo

--- main.php
+++ main.php
@@ -2,4 +2,4 @@
 use madmis\CoingeckoApi\CoingeckoApi;
 
 $api = new CoingeckoApi();
-$timestamp = $api->shared()->priceCharts(Api::BASE_ETH, Api::QUOTE_USD, Api::PERIOD_24HOURS, true));
+$timestamp = $api->shared()->priceCharts(Api::BASE_ETH, Api::QUOTE_USD, Api::PERIOD_24HOURS, true);
