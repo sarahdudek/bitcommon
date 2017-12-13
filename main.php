b3b84a55286ec4895ae2051e9ea2ea62b484208d
sarahdudek <sarah.marie.dudek@gmail.com>
2017-12-12 21:52:50 -0600

    Use Api namespace in main.php

--- main.php
+++ main.php
@@ -1,5 +1,6 @@
 <?php
 use madmis\CoingeckoApi\CoingeckoApi;
+use madmis\CoingeckoApi\Api;
 
 $api = new CoingeckoApi();
 $timestamp = $api->shared()->priceCharts(Api::BASE_ETH, Api::QUOTE_USD, Api::PERIOD_24HOURS, true);
