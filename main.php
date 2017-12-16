a7e79d08334767e3aaec5092b2d4bdb09978e9bc
sarahdudek <sarah.marie.dudek@gmail.com>
2017-12-16 14:04:32 -0600

    Mock up date

--- main.php
+++ main.php
@@ -7,6 +7,10 @@ function getData() {
 	$rows = $api->shared()->priceCharts(Api::BASE_BCC, Api::QUOTE_USD, Api::PERIOD_24HOURS, false);
 	$groups = array();
 	foreach ($rows["stats"] as $row) {
+		$date = $row[0];
+		array_push($groups, $date, "\n");
+	}
+	foreach ($rows["stats"] as $row) {
 		$price = number_format($row[1], 2, '.', ',');
 		array_push($groups, $price);
 	}
