813d1a1b4eb74625004d7929a4a20ac270a5027a
Jeff Fichtner <jeff.fichtner@eightbot.com>
2017-12-16 14:39:54 -0600

    Get type idk?

--- main.php
+++ main.php
@@ -7,9 +7,10 @@ function getData($crypto = Api::BASE_BCC, $currency = Api::QUOTE_USD, $period = 
 	$data = array();
 	foreach ($stats as $row) {
 		$obj = array();
-		$dt = new DateTime($row[0]);
-		$obj["date"] = $dt->format('Y-m-d H:i:s');
+		// $dt = new DateTime($row[0]);
+		$obj["date"] = $row[0];//$dt->format('Y-m-d H:i:s');
 		$obj["price"] = number_format($row[1], 2, '.', ',');
+		$obj["type"] = gettype($row[0]);
 		$data[] = $obj;
 	}
 	return $data;
