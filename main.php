e16a698a53f1d0531d4f0de72a3f358c591b9b60
Jeff Fichtner <jeff.fichtner@eightbot.com>
2017-12-16 14:02:18 -0600

    Pass prices only

--- main.php
+++ main.php
@@ -4,5 +4,15 @@ use madmis\CoingeckoApi\Api;
 
 function getData() {
 	$api = new CoingeckoApi();
-	return $api->shared()->priceCharts(Api::BASE_BCC, Api::QUOTE_USD, Api::PERIOD_24HOURS, false);
+	$rows = $api->shared()->priceCharts(Api::BASE_BCC, Api::QUOTE_USD, Api::PERIOD_24HOURS, false);
+	$groups = array();
+	foreach ($rows["stats"] as $row) {
+		$price = number_format($row[1], 2, '.', ',');
+		array_push($groups, $price);
+	}
+	// $keys = array();
+	// foreach ($rows as $key => $value) {
+	// 	array_push($keys, $price);
+	// }
+	return $groups;
 } 
