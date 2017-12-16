be7ce9b523b3a316626ae3114ce4fd5d0efe07ac
Jeff Fichtner <jeff.fichtner@eightbot.com>
2017-12-16 12:55:46 -0600

    Remove mapping to obj

--- main.php
+++ main.php
@@ -3,7 +3,7 @@ use madmis\CoingeckoApi\CoingeckoApi;
 use madmis\CoingeckoApi\Api;
 
 $api = new CoingeckoApi();
-$timestamp = $api->shared()->priceCharts(Api::BASE_BCC, Api::QUOTE_USD, Api::PERIOD_24HOURS, true);
+$timestamp = $api->shared()->priceCharts(Api::BASE_BCC, Api::QUOTE_USD, Api::PERIOD_24HOURS, false);
 $dumpedObj = var_dump($timestamp, true);
 
 // make a simple object that stores/enumerates through the data
