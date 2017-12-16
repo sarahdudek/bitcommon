5952b804446f8fc849bb2ff17ae2e5f6062a7742
sarahdudek <sarah.marie.dudek@gmail.com>
2017-12-16 11:32:33 -0600

    Update timestamp call and edit view to show variable - hopefully

--- main.php
+++ main.php
@@ -3,5 +3,13 @@ use madmis\CoingeckoApi\CoingeckoApi;
 use madmis\CoingeckoApi\Api;
 
 $api = new CoingeckoApi();
-$timestamp = $api->shared()->priceCharts(Api::BASE_ETH, Api::QUOTE_USD, Api::PERIOD_24HOURS, true);
+$timestamp = $api->shared()->priceCharts(Api::BASE_BCC, Api::QUOTE_USD, Api::PERIOD_24HOURS, true);
 $dumpedObj = var_export($timestamp, true);
+
+// make a simple object that stores/enumerates through the data
+// Number one function:
+	// Function to take that return value, make it pretty
+
+// public function printPricePoint($datePriceObject) {
+// 	return true
+// } 
